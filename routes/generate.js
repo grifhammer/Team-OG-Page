b8f5919524739ae8ffd196aae8867e0478593c0d
Griffin Hammer <grifhammer@gmail.com>
2016-01-28 17:16:45 -0500

    Code creates entries for each player in database.

--- routes/generate.js
+++ routes/generate.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var Members = require('../models/team-members');
+var TeamMember = require('../models/team-members');
 var Team = require('../models/team');
 var vars = require('../config/vars.json');
 var https = require('https');
+var BigNumber = require("bignumber.js");
 
 var steamKey = process.env.PROD_STEAM_KEY || vars.apiKey;
 
 var getTeamBaseUrl = "https://api.steampowered.com/IDOTA2Match_570/GetTeamInfoByTeamID/v001/";
-
+var getPlayerBaseURL = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/"
 
 router.post('/team', function (req, res, next){
-    console.log(req.body);
 
     // Fetch team data
     var getTeamEndpoint = getTeamBaseUrl + "?key="+ steamKey + "&start_at_team_id=" + req.body.teamId + "&teams_requested=1";
@@ -23,6 +23,8 @@ router.post('/team', function (req, res, next){
             console.log(team);
             var playerList = []
             var leagueList = []
+
+            //currently there is no way to know how many players or leagues will be returned
             for(var i = 0; i < 10; i++){
                 thisLeague = "league_id_" + i;
                 thisPlayer = "player_" + i + "_account_id"
@@ -37,7 +39,7 @@ router.post('/team', function (req, res, next){
             console.log(leagueList)
             // Insert team into database
 
-            Team.findOneAndUpdate({}, {  id: parseInt(req.body.teamId),
+            Team.findOneAndUpdate({id: req.body.teamId}, {  id: parseInt(req.body.teamId),
                                                             name: team.name,
                                                             tag: team.tag,
                                                             nationality: team.country_code,
@@ -48,6 +50,48 @@ router.post('/team', function (req, res, next){
                                                                     console.log(error);
                                                                 }
                                                             });
+
+            //Insert players into the database
+            playerList.forEach(function (player){
+                insertPlayer(player);
+            });
+
+            leagueList.forEach(function (league) {
+                insertLeagueMatches(league, req.body.teamId)
+            });
+
+            function insertPlayer(playerId){
+                var steamId = new BigNumber(playerId).plus('76561197960265728');
+                var getPlayerEndpoint = getPlayerBaseURL + "?key=" + steamKey + "&steamids="+ new BigNumber(playerId).plus('76561197960265728')
+                https.get(getPlayerEndpoint, function (res){
+                    res.on('data', function (data){
+                        steamProfile = JSON.parse(data).response.players[0]
+                        TeamMember.findOneAndUpdate({id: playerId}, {fullName: steamProfile.realname,
+                                                                     tag: steamProfile.personaname, 
+                                                                     age: steamProfile.timecreated, 
+                                                                     nationality: steamProfile.loccountrycode, 
+                                                                     position: "1",
+                                                                     image: steamProfile.avatarfull},
+                                                                     {upsert: true},
+                                                                     function (error, result){
+                                                                        // console.log(error);
+                                                                        console.log(result);
+                                                                     })
+
+                    });
+
+                });
+
+            }
+
+            function insertLeagueMatches(leagueId, teamId){
+
+            }
+            //Get league matches
+                // Find ones that match team ID
+                // store these matches in database
+                // Store last parsed match somewhere
+
         });
 
     });
